7b1e4f104c41bfe7da20419aeff350a999f61b16
Gravehg <80481681+Gravehg@users.noreply.github.com>
2024-06-03 22:51:57 -0600

    Code

---
+++ TestApp.py
@@ -0,0 +1,16 @@
+import unittest
+from WebApp import app
+
+class HelloWorldTestCase(unittest.TestCase):
+
+    def setUp(self):
+        self.app = app.test_client()
+        self.app.testing = True
+
+    def test_hello_world(self):
+        response = self.app.get('/')
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(b'Hello, World!', response.data)
+
+if __name__ == '__main__':
+    unittest.main()
